18fd8548734c8dcadc3b4056d9847a93ff74e228
NicolasPicardi <signos97@gmail.com>
2020-11-04 08:13:35 -0300

    basic routing

---
+++ src/screens/SingleScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SingleScreen = () => {
+  return (
+    <View>
+      <Text>Single</Text>
+    </View>
+  )
+}
+
+export default SingleScreen
+
+const styles = StyleSheet.create({})
